705b862cb6ac7f48654c9735e105c978e8b6c991
최상훈 <shchoi@g.seoultech.ac.kr>
2023-09-19 15:54:09 +0900

    Week3

--- Week3/python02_lab/class_score_analysis.py
+++ Week3/python02_lab/class_score_analysis.py
@@ -1,23 +1,35 @@
 def read_data(filename):
     # TODO) Read `filename` as a list of integer numbers
     data = []
+    with open(filename, 'r') as f:
+        for line in f.readlines():
+            if(line.startswith("#")):
+                continue
+            values = [int(word) for word in line.split(',')]
+            data.append(values)
     return data
 
 def calc_weighted_average(data_2d, weight):
     # TODO) Calculate the weighted averages of each row of `data_2d`
     average = []
+    for (m_data, f_data) in data_2d:
+        average.append((m_data * weight[0]) + (f_data * weight[1]))
     return average
 
 def analyze_data(data_1d):
     # TODO) Derive summary of the given `data_1d`
     # Note) Please don't use NumPy and other libraries. Do it yourself.
-    mean = 0
-    var = 0
-    median = 0
+    mean = sum(data_1d) / len(data_1d)
+    vsum = 0
+    for val in data_1d:
+        vsum += (val - mean)**2
+    var = vsum / len(data_1d)
+    sorted_data = sorted(data_1d)
+    median = sorted_data[len(data_1d)//2]
     return mean, var, median, min(data_1d), max(data_1d)
 
 if __name__ == '__main__':
-    data = read_data('class_score_en.csv')
+    data = read_data('data/class_score_en.csv')
     if data and len(data[0]) == 2: # Check 'data' is valid
         average = calc_weighted_average(data, [40/125, 60/100])
 
